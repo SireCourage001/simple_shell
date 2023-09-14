5175dbf38899ac1ba1cf7a44b3007e6899baa6f1
Evarmedia <mishakmanuel@gmail.com>
2023-09-14 14:08:26 +0000

    testfiles

---
+++ test_funcs/main_shell.c
@@ -0,0 +1,121 @@
+#include "shell.h"
+
+/**
+ * handle_ctrl_c - print the prompt in a new line
+ * when the signal SIGINT (ctrl + c) is send to the program
+ * @UNUSED: option of the prototype
+ */
+void ctrl_plus_c_handler(int opr UNUSED)
+{
+	_print("\n");
+	_print(PROMPT_MSG);
+}
+
+/**
+ * sisifo - its a infinite loop that shows the prompt
+ * @prompt: prompt to be printed
+ * @data: its a infinite loop that shows the prompt
+ */
+void system_info_func(char *prompt, core_prog_data *data)
+{
+	int error_code = 0, string_len = 0;
+
+	while (++(data->exec_counter))
+	{
+		_print(prompt);
+		error_code = string_len = _togetline(data);
+
+		if (error_code == EOF)
+		{
+			free_all_data(data);
+			exit(errno);
+		}
+		if (string_len >= 1)
+		{
+			expand_alias(data);
+			expand_variables(data);
+			tokenizerfuncn(data);
+			if (data->tokens[0])
+			{ /* if a text is given to prompt, execute */
+				error_code = execute(data);
+				if (error_code != 0)
+					_print_error(error_code, data);
+			}
+			free_recurrent_data(data);
+		}
+	}
+}
+
+/**
+ * inicialize_data - inicialize the struct with the info of the program
+ * @data: pointer to the structure of data
+ * @argv: array of arguments pased to the program execution
+ * @env: environ pased to the program execution
+ * @argc: number of values received from the command line
+ */
+void init_data_func(core_prog_data *data, int argc, char *argv[], char **env)
+{
+	int i = 0;
+
+	data->program_name = argv[0];
+	data->input_line = NULL;
+	data->command_name = NULL;
+	data->exec_counter = 0;
+	if (argc == 1)
+		data->file_descriptor = STDIN_FILENO;
+	else
+	{
+		data->file_descriptor = open(argv[1], O_RDONLY);
+		if (data->file_descriptor == -1)
+		{
+			_printit(data->program_name);
+			_printit(": 0: Can't open ");
+			_printit(argv[1]);
+			_printit("\n");
+			exit(127);
+		}
+	}
+	data->tokens = NULL;
+	data->env = malloc(sizeof(char *) * 50);
+	if (env)
+	{
+		for (; env[i]; i++)
+		{
+			data->env[i] = str_duplicate(env[i]);
+		}
+	}
+	data->env[i] = NULL;
+	env = data->env;
+
+	data->alias_list = malloc(sizeof(char *) * 20);
+	for (i = 0; i < 20; i++)
+	{
+		data->alias_list[i] = NULL;
+	}
+}
+
+/**
+ * main - initialize the main program
+ * @argc: number of values received
+ * @argv: values from the command line
+ * @env: values received from the com line
+ * Return: Returns zero on succes.
+ */
+int main(int argc, char *argv[], char *env[])
+{
+	core_prog_data data_struct = {NULL}, *data = &data_struct;
+	char *prompt = "";
+
+	init_data_func(data, argc, argv, env);
+
+	signal(SIGINT, ctrl_plus_c_handler);
+
+	if (isatty(STDIN_FILENO) && isatty(STDOUT_FILENO) && argc == 1)
+	{
+		errno = 2;
+		prompt = PROMPT_MSG;
+	}
+	errno = 0;
+	system_info_func(prompt, data);
+	return (0);
+}
---
+++ test_funcs/shell.h
@@ -0,0 +1,109 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include "macros.h"
+
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <errno.h>
+#include <stddef.h>
+#include <fcntl.h>
+
+/**
+ * struct info- struct for the program's data
+ * @program_name: the name of the executable
+ * @input_line: pointer to the input read for _getline
+ * @command_name: pointer to the first command typed by the user
+ * @exec_counter: number of excecuted comands
+ * @file_descriptor: file descriptor to the input of commands
+ * @tokens: pointer to array of tokenized input
+ * @env: copy of the environ
+ * @alias_list: array of pointers with aliases.
+ */
+typedef struct info
+{
+	char *program_name;
+	char *input_line;
+	char *command_name;
+	int exec_counter;
+	int file_descriptor;
+	char **tokens;
+	char **env;
+	char **alias_list;
+} core_prog_data;
+
+int character_counter(char *string, char *character);
+int func_toprint_alias(core_prog_data *data, char *alias);
+char *main_get_alias(core_prog_data *data, char *alias);
+int main_set_alias(char *alias_string, core_prog_data *data);
+void long_to_string(long number, char *string, int base);
+int _atoi(char *s);
+
+int str_comparingfunc(char *string1, char *string2, int number);
+int get_str_length(char *string);
+char *str_duplicate(char *string);
+char *str_concator(char *string1, char *string2);
+void str_reversal(char *string);
+
+int _print(char *string);
+int _printit(char *string);
+int _print_error(int errorcode, core_prog_data *data);
+
+int builtin_env(core_prog_data *data);
+int builtin_set_env(core_prog_data *data);
+int builtin_unset_env(core_prog_data *data);
+
+char *env_get_key(char *name, core_prog_data *data);
+int env_set_key(char *key, char *value, core_prog_data *data);
+int env_remove_key(char *key, core_prog_data *data);
+void print_environ(core_prog_data *data);
+
+int builtin_exit(core_prog_data *data);
+int builtin_cd(core_prog_data *data);
+int set_working_directory(core_prog_data *data, char *new_dir);
+int builtin_helper(core_prog_data *data);
+int builtin_alias(core_prog_data *data);
+
+void free_array_of_pointers(char **directories);
+void free_recurrent_data(core_prog_data *data);
+void free_all_data(core_prog_data *data);
+
+int builtins_list(core_prog_data *data);
+
+char **tokenize_path(core_prog_data *data);
+int find_program(core_prog_data *data);
+
+int execute(core_prog_data *data);
+
+void expand_variables(core_prog_data *data);
+void expand_alias(core_prog_data *data);
+int buffer_add(char *buffer, char *str_to_add);
+
+void tokenizerfuncn(core_prog_data *data);
+char *_strtok(char *line, char *delim);
+
+int _togetline(core_prog_data *data);
+int check_logic_ops(char *array_commands[], int i, char array_operators[]);
+
+void init_data_func(core_prog_data *data, int arc, char *argv[], char **env);
+void system_info_func(char *prompt, core_prog_data *data);
+void ctrl_plus_c_handler(int opr UNUSED);
+
+/**
+ * struct builtins - struct for the builtins
+ * @builtin: the name of the builtin
+ * @function: the associated function to be called for each builtin
+ */
+typedef struct builtins
+{
+	char *builtin;
+	int (*function)(core_prog_data *data);
+} builtins;
+
+#endif /*SHELL_H*/
